f36b025067c7732accbf1d9e5fcdf9bc90dd6980
songzhukai <songzhukai@le.com>
2019-11-27 12:43:27 +0800

    update

---
+++ src/main/java/reflect/ParameterizedTypeDemo.java
@@ -0,0 +1,24 @@
+package reflect;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+public class ParameterizedTypeDemo {
+
+    public static void main(String[] args) {
+        Foo<String> foo = new Foo<String>() {};
+        // 在类的外部这样获取
+        Type type = ((ParameterizedType) foo.getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+        System.out.println(type);
+        // 在类的内部这样获取
+        System.out.println(foo.getTClass());
+    }
+
+
+    abstract static class Foo<T> {
+        public Class<T> getTClass() {
+            Class<T> tClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+            return tClass;
+        }
+    }
+}
